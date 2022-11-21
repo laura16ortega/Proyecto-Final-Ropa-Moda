7eec84f059bba33011dfa472ea1b91061194b0cf
Felipe Morales <carrionfelipe15@gmail.com>
2022-11-21 17:10:36 -0500

    SearchBar

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import Test from './components/Test/Test';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Search from "./components/Search/Search";
+import "./App.css";
 
 function App() {
-   return (
-      <div className="App">
-         <Routes>
-            <Route path='/' element={<Test />} />
-         </Routes>
-      </div>
-   );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Search />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
