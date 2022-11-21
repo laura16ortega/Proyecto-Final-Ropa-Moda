64534c320d150e74dec3dd1cd2c85f42821008b8
Laura <96241880+laura16ortega@users.noreply.github.com>
2022-11-21 17:39:05 -0500

    Revert "Add Search Bar"

--- Client/src/assets/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux'
-import type { TypedUseSelectorHook } from 'react-redux'
-import type { RootState, AppDispatch } from '../../redux/store'
-
-// Usarlos en vez de useDispatch y useSelector
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
