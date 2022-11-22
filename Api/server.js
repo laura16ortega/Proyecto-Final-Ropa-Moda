a8f1efbe7b266faf98e46e30c81e0d459c238cd5
santiago hurtado <sahurtaro@gmail.com>
2022-11-22 10:24:25 -0500

    modelos user y product

--- Api/server.js
+++ Api/server.js
@@ -21,7 +21,7 @@ mongoose
 
 //START THE SERVER
 // console.log(process.env);
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Running on port ${port}...`);
 }); //Así se inicializa el servidor, recibe como parámetros el puerto y un callback
