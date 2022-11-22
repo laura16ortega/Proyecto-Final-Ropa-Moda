3c82c2aa136ca1c6ce2294b635704f029e001752
NicolasVeron <veronnicolas2002@gmail.com>
2022-11-22 14:05:59 -0300

    Cards

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import Test from './components/Test/Test';
+import Home from './pages/Home';
 import './App.css';
+import Cart from './components/Cart/Cart';
 
 function App() {
    return (
       <div className="App">
          <Routes>
-            <Route path='/' element={<Test />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
          </Routes>
       </div>
    );
