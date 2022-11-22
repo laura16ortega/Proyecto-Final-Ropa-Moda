8b1a655c89b8aaca86dd78ecd36d67cea326c34e
Laura <96241880+laura16ortega@users.noreply.github.com>
2022-11-22 15:15:31 -0500

    .

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,17 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import Test from './components/Test/Test';
+// import Test from './components/Test/Test';
 import Search from "./components/Search/Search";
 import './App.css';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
    return (
       <div className="App">
          <Routes>
             <Route path="/search" element={<Search />} />
-            <Route path='/' element={<Test />} />
+            {/* <Route path='/' element={<Test />} /> */}
+            <Route path='/landingPage' element={<LandingPage />} />
          </Routes>
-         <Footer />
+         {/* <Footer /> */}
       </div>
    );
 
