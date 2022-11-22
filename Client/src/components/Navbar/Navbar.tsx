818a94e895054c70687ce8649d6a5b3f0acbb28b
aribxax <aribxax@gmail.com>
2022-11-23 00:18:14 +0100

    nav

--- Client/src/components/Navbar/Navbar.tsx
+++ Client/src/components/Navbar/Navbar.tsx
@@ -29,7 +29,7 @@ export const Navbar = () => {
                         }}
                         color='inherit'
                         >
-                        Home
+                        Homee
                         </Button>
 
                         <Button 
