6a8d4427026193dc929a0d6860aa8ef622fc39e2
NicolasVeron <veronnicolas2002@gmail.com>
2022-11-23 12:13:39 -0300

    Appjs conflict fix

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import Search from "./components/Search/Search";
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import SignInSide from './pages/Login/SignInSide';
 import './App.css';
 
 function App() {
