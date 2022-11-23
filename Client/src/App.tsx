9f3e9bed6b7aa233c46c92efd1e646bb7e6b2d86
aribxax <aribxax@gmail.com>
2022-11-23 15:34:07 +0100

    sign in page and navbar buttons

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -3,13 +3,16 @@ import Home from './pages/Home';
 import Search from "./components/Search/Search";
 import './App.css';
 import Footer from './components/Footer/Footer';
-
+import Navbar from './components/Navbar/Navbar';
+import SignInSide from './pages/Login/SignInSide';
 function App() {
    return (
       <div className="App">
+         <Navbar />
          <Routes>
             <Route path='/' element={<Home />} />
             <Route path="/search" element={<Search />} />
+            <Route path='/login' element={<SignInSide />} />
          </Routes>
          <Footer />
       </div>
