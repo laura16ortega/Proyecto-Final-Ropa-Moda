7277c3db0f704897e4f82d38a46d1be476fa4f79
Felipe Morales <carrionfelipe15@gmail.com>
2022-11-23 09:51:23 -0500

    route detail card

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,20 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Search from "./components/Search/Search";
-import './App.css';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import DetailCard from "./components/DetailCard/DetailCard";
 
 function App() {
-   return (
-      <div className="App">
-         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path="/search" element={<Search />} />
-         </Routes>
-         <Footer />
-      </div>
-   );
-
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/home/:id" element={<DetailCard />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
