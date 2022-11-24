674f91e4d307a7448e1451ec375bfa3c93f540de
santiago hurtado <sahurtaro@gmail.com>
2022-11-23 22:41:50 -0500

    Ruta post, patch y delete producto. Propiedad talla en modelo producto

--- Api/controllers/productController.js
+++ Api/controllers/productController.js
@@ -29,6 +29,40 @@ exports.getProduct = async (req, res) => {
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
   }
+};
 
-  //   if (id > tours.length) {
+exports.createProduct = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: { product: newProduct },
+    });
+  } catch (err) {
+    res.status(400).json({ status: "fail,", message: err });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const tour = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      //el segundo parámetro es la información con la que queremos cambiar el tour
+      new: true, //el tercer parámetro indica que queremos devolver el documento modíficado en vez del original
+      runValidators: true,
+    });
+    res.status(200).json({ status: "success", data: { tour: tour } });
+  } catch (err) {
+    res.status(404).json({ status: "fail", message: err });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const tour = await Product.findByIdAndDelete(req.params.id);
+    res
+      .status(204) //204 significa "no content"
+      .json({ status: "success", data: null }); // ya no enviamos datos sino que enviamos null
+  } catch (err) {
+    res.status(404).json({ status: "fail", message: err });
+  }
 };
--- Api/routes/productRoutes.js
+++ Api/routes/productRoutes.js
@@ -4,8 +4,15 @@ const router = express.Router();
 
 //ROUTES
 
-router.route("/").get(productController.getAllProducts);
+router
+  .route("/")
+  .get(productController.getAllProducts)
+  .post(productController.createProduct);
 
-router.route("/:id").get(productController.getProduct);
+router
+  .route("/:id")
+  .get(productController.getProduct)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
 
 module.exports = router;
