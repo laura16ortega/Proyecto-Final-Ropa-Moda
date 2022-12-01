216ddb64b64927abcaf9a0ecd926db54e0d14a4f
santiago hurtado <sahurtaro@gmail.com>
2022-11-30 19:31:37 -0500

    manejar errores de rutas no manejadas

--- Api/app.js
+++ Api/app.js
@@ -4,23 +4,18 @@ const productRouter = require("./routes/productRoutes");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
-const {auth} = require("express-openid-connect");
+const { auth } = require("express-openid-connect");
 const { config } = require("./services/authServices");
 const cookieParser = require("cookie-parser");
-require('./services/googleAuthServices');
-
+require("./services/googleAuthServices");
 
 const app = express();
-
-
-
 app.use(express.json()); //Middleware para que express pueda leer lo que viene por req.body. El método use se usa para usar middleware
 app.use(cors());
 app.use(cookieParser());
 app.use(auth(config));
- 
-//Routes
 
+//Routes
 
 app.use("/api/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
@@ -30,10 +25,16 @@ app.use((req, res, next) => {
   next();
 });
 
-
 //ROUTES
 app.use("/api/v1/products", productRouter); //middleware
 // app.use("/api/v1/users", userRouter); //middleware
 app.use(express.static(`${__dirname}/public`));
-module.exports = app;
 
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
+module.exports = app;
