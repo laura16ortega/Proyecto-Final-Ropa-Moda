dc391e9031029f307e402af6406af271369f07aa
Laura <96241880+laura16ortega@users.noreply.github.com>
2022-12-09 18:23:53 -0500

    enmaquetado finalizado

--- Client/src/components/Dashboard/MainDashboard/MainDashboard.jsx
+++ Client/src/components/Dashboard/MainDashboard/MainDashboard.jsx
@@ -5,6 +5,8 @@ import Home from "../home/Home";
 
 
 
+
+
 function Dashboard() {
     return (
         <div>
--- Client/src/components/Dashboard/chart/Chart.jsx
+++ Client/src/components/Dashboard/chart/Chart.jsx
@@ -18,36 +18,3 @@ export default function Chart({title, data, dataKey, grid}) {
     </div>
   )
 }
-
-
-
-// {
-//     name: 'Page B',
-//     "Active User": 3000,
-  
-//   },
-//   {
-//     name: 'Page C',
-//     "Active User": 2000,
-
-//   },
-//   {
-//     name: 'Page D',
-//     "Active User": 2780,
-
-//   },
-//   {
-//     name: 'Page E',
-//     "Active User": 1890,
-
-//   },
-//   {
-//     name: 'Page F',
-//     "Active User": 2390,
-
-//   },
-//   {
-//     name: 'Page G',
-//     "Active User": 3490,
-
-//   },
