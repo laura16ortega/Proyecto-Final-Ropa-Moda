7093ad706f455b5bd40de7a6ef002ac8a60a82ee
Felipe Morales <carrionfelipe15@gmail.com>
2022-12-12 17:49:13 -0500

    Filters Responsive

--- Client/src/components/Filters/Filters.tsx
+++ Client/src/components/Filters/Filters.tsx
@@ -115,107 +115,120 @@ const SortFilter = () => {
   }, [filters]);
 
   return (
-    <Box>
+    <>
+      <Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingTop: "0.5rem",
+            marginTop: "1rem",
+            marginBottom: "0.5rem",
+            borderTop: "1px solid rgb(224, 222, 220)",
+          }}
+        >
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography variant="h6" sx={{ marginRight: "1.5rem" }}>
+              Filtros:
+            </Typography>
+            <Box className={s.filtersContainer}>
+              {filterTypes.map((e) => (
+                <FilterPopup
+                  key={e.name}
+                  filterDetails={e}
+                  filters={filters}
+                  setFilters={setFilters}
+                />
+              ))}
+              {conditionalSizes.map((e) => (
+                <ConditionalRendering
+                  key={e.name}
+                  filterDetails={e}
+                  filters={filters}
+                  setFilters={setFilters}
+                />
+              ))}
+            </Box>
+          </Box>
+        </Box>
+        <Collapse in={activeFilters.length > 0}>
+          <Box
+            sx={{ display: "flex", alignItems: "center", marginBottom: "2rem" }}
+          >
+            <Box
+              sx={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}
+            >
+              {arr.map((e, i) => (
+                <Box key={i + 1} className={s.selectedFiltersContainer}>
+                  <Typography variant="subtitle2" letterSpacing={0.6}>
+                    {e.value}
+                  </Typography>
+                  <ClearIcon
+                    sx={{ marginLeft: ".5rem", cursor: "pointer" }}
+                    onClick={(event) =>
+                      handleDeleteFilter(e.key as string, e.value as string)
+                    }
+                  />
+                </Box>
+              ))}
+            </Box>
+            <Box sx={{ marginLeft: "1.5rem" }}>
+              {!activeFilters.length ? (
+                ""
+              ) : (
+                <Typography
+                  variant="subtitle1"
+                  onClick={(e) => handleFiltersReset(e)}
+                  sx={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  Remover filtros
+                </Typography>
+              )}
+            </Box>
+          </Box>
+        </Collapse>
+      </Box>
       <Box
         sx={{
           display: "flex",
-          justifyContent: "space-between",
+          marginBottom: "2rem",
           alignItems: "center",
           paddingY: "0.5rem",
-          marginY: "2rem",
           borderTop: "1px solid rgb(224, 222, 220)",
           borderBottom: "1px solid rgb(224, 222, 220)",
         }}
       >
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Typography variant="h6" sx={{ marginRight: "1.5rem" }}>
-            Filtros:
-          </Typography>
-          <Box className={s.filtersContainer}>
-            {filterTypes.map((e) => (
-              <FilterPopup
-                key={e.name}
-                filterDetails={e}
-                filters={filters}
-                setFilters={setFilters}
-              />
+        <Typography variant="h6" sx={{ marginRight: "1rem" }}>
+          Orden:
+        </Typography>
+        <Box>
+          <Select
+            displayEmpty
+            value={sort}
+            onChange={(e) => handleSort(e)}
+            className={s.selectSort}
+            MenuProps={{
+              anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "right",
+              },
+              transformOrigin: {
+                vertical: "top",
+                horizontal: "right",
+              } /* Para que aparezca desde arriba a la derecha en vez del centro :s */,
+            }}
+          >
+            <MenuItem value="default">Por defecto</MenuItem>
+            {sortTypes.map((e) => (
+              <MenuItem key={e.value} value={e.value}>
+                {e.name}
+              </MenuItem>
             ))}
-            {conditionalSizes.map((e) => (
-              <ConditionalRendering
-                key={e.name}
-                filterDetails={e}
-                filters={filters}
-                setFilters={setFilters}
-              />
-            ))}
-          </Box>
-        </Box>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Typography variant="h6" sx={{ marginRight: "1rem" }}>
-            Orden:
-          </Typography>
-          <Box>
-            <Select
-              displayEmpty
-              value={sort}
-              onChange={(e) => handleSort(e)}
-              className={s.selectSort}
-              MenuProps={{
-                anchorOrigin: {
-                  vertical: "bottom",
-                  horizontal: "right",
-                },
-                transformOrigin: {
-                  vertical: "top",
-                  horizontal: "right",
-                } /* Para que aparezca desde arriba a la derecha en vez del centro :s */,
-              }}
-            >
-              <MenuItem value="default">Por defecto</MenuItem>
-              {sortTypes.map((e) => (
-                <MenuItem key={e.value} value={e.value}>
-                  {e.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </Box>
+          </Select>
         </Box>
       </Box>
-      <Collapse in={activeFilters.length > 0}>
-        <Box
-          sx={{ display: "flex", alignItems: "center", marginBottom: "2rem" }}
-        >
-          <Box sx={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
-            {arr.map((e, i) => (
-              <Box key={i + 1} className={s.selectedFiltersContainer}>
-                <Typography variant="subtitle2" letterSpacing={0.6}>
-                  {e.value}
-                </Typography>
-                <ClearIcon
-                  sx={{ marginLeft: ".5rem", cursor: "pointer" }}
-                  onClick={(event) =>
-                    handleDeleteFilter(e.key as string, e.value as string)
-                  }
-                />
-              </Box>
-            ))}
-          </Box>
-          <Box sx={{ marginLeft: "1.5rem" }}>
-            {!activeFilters.length ? (
-              ""
-            ) : (
-              <Typography
-                variant="subtitle1"
-                onClick={(e) => handleFiltersReset(e)}
-                sx={{ cursor: "pointer", textDecoration: "underline" }}
-              >
-                Remover filtros
-              </Typography>
-            )}
-          </Box>
-        </Box>
-      </Collapse>
-    </Box>
+    </>
   );
 };
 
